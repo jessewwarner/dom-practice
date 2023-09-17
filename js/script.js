f347e8d4ad7ef6e4394034730e7dcb8653052243
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-17 18:59:18 -0400

    Add practice content from TOP

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
-content.classList.add('content');
+content.classList.toggle('content');
 content.textContent = 'This is the text inside the "div" element created with JS';
 
-const contentDiv = document.querySelector('.content');
-const moreContent = document.createElement('p');
-moreContent.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit quae a maxime magnam sit eaque sunt cupiditate quis eum. Pariatur nihil distinctio necessitatibus dolor nobis provident natus nesciunt repellat magni.'
+const redParagraph = document.createElement('p');
+redParagraph.textContent = "Hey I'm red!";
+redParagraph.style.color = 'red';
+
+const headerThree = document.createElement('h3');
+headerThree.textContent = "I'm a blue h3!";
+headerThree.style['color'] = 'blue';
+
+const newDiv = document.createElement('div');
+newDiv.style = 'border: 2px solid black; background-color: pink';
+
+const headerOne = document.createElement('div');
+headerOne.textContent = "I'm in a div";
+
+const newDivParagraph = document.createElement('p');
+newDivParagraph.textContent = "ME TOO!"
+
+newDiv.appendChild(headerOne);
+newDiv.appendChild(newDivParagraph);
 
 container.appendChild(content);
-container.appendChild(moreContent);
+container.appendChild(redParagraph);
+container.appendChild(headerThree);
+container.appendChild(newDiv);
