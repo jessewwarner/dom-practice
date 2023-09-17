880df6f1388763cbe422f502e132b35e93eee431
Jesse Warner <jesse.w.warner@gmail.com>
2023-09-17 18:43:37 -0400

    Add a new paragraph with placeholder text

--- js/script.js
+++ js/script.js
@@ -4,4 +4,9 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the text inside the "div" element created with JS';
 
+const contentDiv = document.querySelector('.content');
+const moreContent = document.createElement('p');
+moreContent.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit quae a maxime magnam sit eaque sunt cupiditate quis eum. Pariatur nihil distinctio necessitatibus dolor nobis provident natus nesciunt repellat magni.'
+
 container.appendChild(content);
+container.appendChild(moreContent);
